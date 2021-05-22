11ee1ae0ba7c826e0f49ff4171af80d73b00a326
FatalVindicator <bamarandino@gmail.com>
2021-05-22 16:34:44 -0400

    Fices empty string not returning None in conv_num. Added tests for my_datetime

--- tests.py
+++ tests.py
@@ -1,10 +1,22 @@
 import unittest
+from task import conv_num, my_datetime, conv_endian
+import random
+import time
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    # Test my_datetime for 0 seconds
+    def test_datetime1(self):
+        self.assertEqual('01-01-1970', my_datetime(0))
+
+    # Test my_datetime for leap year
+    def test_datetime2(self):
+        self.assertEqual('06-06-1972', my_datetime(76680000))
+
+    # Test my_datetime for 2/29 during leap year
+    def test_datetime3(self):
+        self.assertEqual('02-29-1972', my_datetime(68212800))
 
 
 if __name__ == '__main__':
