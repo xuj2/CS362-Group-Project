b9b38339f0d4330da76a1017cd6231bedf15b439
Patrick Godfrey <godfrepa@oregonstate.edu>
2021-05-23 21:24:24 +0900

    Fixed some linting errors in tests.py

--- tests.py
+++ tests.py
@@ -30,6 +30,7 @@ def build_test_func(expected, test_case, func_under_test, message):
         self.assertEqual(expected, result, message.format(test_case, expected, result))
     return test
 
+
 def generate_tests_conv_num(generate=10000):
     # Generates test cases for conv_num
     message = 'Test case: {}, Expected: {}, Result: {}'
@@ -55,17 +56,17 @@ def generate_tests_conv_num(generate=10000):
     for _ in range(generate):
         # Tests if conv_num returns none for random strings of numbers and letters
         # Maybe this should be changed? Possibility of getting an actual hex number exists
-        input = ''.join(random.choices(string.ascii_letters + string.digits, k = random.randint(10, 15)))
-        new_test = build_test_func(None, input, conv_num, message)
-        setattr(RandomTestCase, 'test_{}'.format(input), new_test)
+        test_input = ''.join(random.choices(string.ascii_letters + string.digits, k=random.randint(10, 15)))
+        new_test = build_test_func(None, test_input, conv_num, message)
+        setattr(RandomTestCase, 'test_{}'.format(test_input), new_test)
 
     for _ in range(generate):
         # Tests if conv_num returns none for a number containing 2 decimals
         num = random.randint(-10000000, 10000000)
-        input, index = str(num), random.randint(1, len(str(num))-1)
-        input = input[:index] + '..' + input[index:]
-        new_test = build_test_func(None, input, conv_num, message)
-        setattr(RandomTestCase, 'test_{}'.format(input), new_test)
+        test_input, index = str(num), random.randint(1, len(str(num))-1)
+        test_input = test_input[:index] + '..' + test_input[index:]
+        new_test = build_test_func(None, test_input, conv_num, message)
+        setattr(RandomTestCase, 'test_{}'.format(test_input), new_test)
 
 
 if __name__ == '__main__':
